3e0dfd5558300d7033119055306ca44c5a0bd32b
V. Andrew <v.andrew@live.com>
2020-11-07 12:59:30 -0500

    Added Delay function

--- src/helloWorld.ts
+++ src/helloWorld.ts
@@ -1,3 +1,7 @@
+import { delay } from "./lib/common"
+
 export async function helloWorld() {
-    console.log("Hello World!")
+    console.log("Before delay!")
+    await delay(500)
+    console.log("After delay\nHello World Async!")
 }
